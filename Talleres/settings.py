384274612f387ead5683a5f66984cba330c7057b
Juan Collado <purplefox.j@gmail.com>
2015-04-16 11:00:08 -0300

    Se agrega bootstrap con un menu de prueba y una página de inicio

--- Talleres/settings.py
+++ Talleres/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap',
+)
+
+TEMPLATE_DIRS = (
+    'Talleres/templates/',
 )
 
 MIDDLEWARE_CLASSES = (
