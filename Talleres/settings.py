87e746dd73bf3892ba628ce6d69c497e67074cbc
Juan Collado <purplefox.j@gmail.com>
2015-04-16 16:09:59 -0300

    Se agregan los templates para mostrar los mensajes de información, error, etc.

--- Talleres/settings.py
+++ Talleres/settings.py
@@ -86,3 +86,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from django.contrib import messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
--- Talleres/urls.py
+++ Talleres/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from .views import HomeView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Talleres.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', HomeView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
