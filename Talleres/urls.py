14e2573403d3c9799a6dd11a3d3ccaa559a75299
Juan Collado <purplefox.j@gmail.com>
2015-04-17 15:29:32 -0300

    Se arreglan problemas con las urls

--- Talleres/urls.py
+++ Talleres/urls.py
@@ -3,10 +3,6 @@ from django.contrib import admin
 from .views import HomeView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Talleres.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', HomeView.as_view(), name='index'),
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^clientes/', include('clientes.urls', namespace='clientes')),
--- clientes/urls.py
+++ clientes/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.ClienteListView.as_view()),
-    url(r'cliente/(?P<pk>[0-9]+)/$', views.ClienteDetailView.as_view(), name='cliente_detail'),
-    url(r'new/$', views.ClienteCreateView.as_view(), name='cliente_create'),
-    url(r'update/(?P<pk>[0-9]+)/$', views.ClienteUpdateView.as_view(), name='cliente_update'),
-    url(r'$', views.ClienteListView.as_view(), name='cliente_list'),
+    url(r'^$', views.ClienteListView.as_view(), name='cliente_list'),
+    url(r'^cliente/(?P<pk>[0-9]+)/$', views.ClienteDetailView.as_view(), name='cliente_detail'),
+    url(r'^new/$', views.ClienteCreateView.as_view(), name='cliente_create'),
+    url(r'^update/(?P<pk>[0-9]+)/$', views.ClienteUpdateView.as_view(), name='cliente_update'),
 ]
