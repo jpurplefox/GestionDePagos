3e453bc092c7347dbc12275367de0a3102d70817
Juan Collado <purplefox.j@gmail.com>
2015-04-17 15:05:59 -0300

    Se agregan mensajes de confirmación luego de guardar o actualizar

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView, DetailView
+from django.contrib import messages
+
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -10,10 +12,20 @@ class ClienteCreateView(CreateView):
     form_class = ClienteForm
     template_name = 'clientes/cliente_form.html'
 
+    def form_valid(self, form):
+        result = super(ClienteCreateView, self).form_valid(form)
+        messages.success(self.request, "El cliente {nombre} fue creado con éxito".format(nombre=self.object))
+        return result
+
 class ClienteUpdateView(UpdateView):
     model = Cliente
     form_class = ClienteForm
     template_name = 'clientes/cliente_form.html'
+
+    def form_valid(self, form):
+        result = super(ClienteUpdateView, self).form_valid(form)
+        messages.success(self.request, "El cliente {nombre} fue actualizado con éxito".format(nombre=self.object))
+        return result
 
 class ClienteDetailView(DetailView):
     model = Cliente
