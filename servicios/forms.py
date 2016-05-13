77e46f58bf543e095cb3501781b106b39838a040
jpurplefox <collado.juanmaria@gmail.com>
2016-05-13 00:18:40 -0300

    Se pone el checkbox del formulario de servicios para que esté en la misma linea del label

--- servicios/forms.py
+++ servicios/forms.py
@@ -7,5 +7,5 @@ class ServicioForm(forms.ModelForm):
         fields = ('descripcion', 'se_cobra_por_hora')
         widgets = {
             'descripcion': forms.TextInput(attrs={'class': 'form-control'}),
-            'se_cobra_por_hora': forms.CheckboxInput(attrs={'class': 'form-control'})
+            'se_cobra_por_hora': forms.CheckboxInput(attrs={'class': ''})
         }
