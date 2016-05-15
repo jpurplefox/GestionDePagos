8e8d81721d65782fb26b4982bf1af13ddc22468b
jpurplefox <collado.juanmaria@gmail.com>
2016-05-15 18:43:46 -0300

    Cambios en las url del proyecto

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
 	#Clientes
-    url(r'^clientes/$', views.ClienteListView.as_view(), name='cliente_list'),
-    url(r'^cliente/(?P<pk>[0-9]+)/$', views.ClienteDetailView.as_view(), name='cliente_detail'),
-    url(r'^cliente/(?P<pk>[0-9]+)/edit/$', views.ClienteUpdateView.as_view(), name='cliente_update'),
-    url(r'^cliente/new/$', views.ClienteCreateView.as_view(), name='cliente_create'),
-
-    url(r'^json/clientes/$', views.clientes_json, name='cliente_list_json'),
+    url(r'^$', views.ClienteListView.as_view(), name='cliente_list'),
+    url(r'^/(?P<pk>[0-9]+)$', views.ClienteDetailView.as_view(), name='cliente_detail'),
+    url(r'^/(?P<pk>[0-9]+)/edit$', views.ClienteUpdateView.as_view(), name='cliente_update'),
+    url(r'^/new$', views.ClienteCreateView.as_view(), name='cliente_create'),
 ]
--- servicios/urls.py
+++ servicios/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 	#Servicios
-    url(r'^servicios/$', views.ServicioListView.as_view(), name='servicio_list'),
-    url(r'^servicio/(?P<pk>[0-9]+)/$', views.ServicioDetailView.as_view(), name='servicio_detail'),
-    url(r'^servicio/(?P<pk>[0-9]+)/edit/$', views.ServicioUpdateView.as_view(), name='servicio_update'),
-    url(r'^servicio/new/$', views.ServicioCreateView.as_view(), name='servicio_create'),
+    url(r'^$', views.ServicioListView.as_view(), name='servicio_list'),
+    url(r'^/(?P<pk>[0-9]+)$', views.ServicioDetailView.as_view(), name='servicio_detail'),
+    url(r'^/(?P<pk>[0-9]+)/edit$', views.ServicioUpdateView.as_view(), name='servicio_update'),
+    url(r'^/new$', views.ServicioCreateView.as_view(), name='servicio_create'),
 ]
